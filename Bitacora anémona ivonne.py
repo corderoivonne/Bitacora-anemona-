66037bc4a41c931e175e1fdf6887a1a42900b7ac
corderoivonne <51940473+corderoivonne@users.noreply.github.com>
2019-07-30 16:49:06 -0500

    Add files via upload

---
+++ Bitacora anémona ivonne.py
@@ -0,0 +1,279 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  # Anémona _Bunodosoma sp._
+# ### Cordero Ramos Dulce Ivonne 
+
+# <p align="center"><img src!="https://www.google.com/search?q=bunodosoma+sp&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjs0IPWvbfjAhUFIKwKHfnXCjUQ_AUIECgB&biw=1366&bih=625#imgrc=VR-0LvWCEp4HLM:" width = 10%></p>
+
+# <p align="center"><img src!="https://www.google.com/search?q=bunodosoma+sp&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjs0IPWvbfjAhUFIKwKHfnXCjUQ_AUIECgB&biw=1366&bih=625#imgrc=VR-0LvWCEp4HLM:" width = 10%></p>
+
+# ![an%C3%A9mona.jpg](attachment:an%C3%A9mona.jpg) 
+# Figura 1. Fotografia de anémona 
+
+# Las anémonas pertenecen al Phylum Cnidaria, en el cual también se encuentran las medusas, los corales y las hidras, este grupo se caracteriza por tener células urticantes llamadas cnidocitos, los cuales segregan toxinas, como las actinoporinas que actúan haciendo poros en membrana plasmática, citolisinas que actúan en la célula causando lisis (ruptura de membrana), o destrucción de eritrocitos hemolisinas. Estos organismos representan una alternativa interesante de estudio, son una fuente de principios bioactivos y pueden desarrollar un papel importante en el campo de la química, bioquímica, farmacología y biotecnología, también nos pueden proporcionar información sobre procesos de regeneración celular. Las clasificaciones basadas con caracteres morfológicos no siempre coinciden con las basadas con secuencias nucleotídicas (Morandini 2012). Para la identificación de especies de anémonas no basta con la clasificación morfológica, ya que la identificación de la mayoría de los hexacorales de cuerpo blando requiere exámenes morfológicos e histológicos de especímenes preservados y experiencia para la correcta interpretación de las características observadas (Haussermann 2004).
+# Por lo que son necesarios estudios:  
+# + Morfofisiológicos
+# + Histológicos 
+# + Moleculares 
+# 
+# 
+
+# # MÉTODO#
+
+# Se colectaron 10 organismos en el estado de Tecolutla Veracruz
+
+# ![Tecolulta_Mapa%20M.%20en%20C.%20Miguel%20%C3%81ngel%20Le%C3%B3n%20Tapia.jpg](attachment:Tecolulta_Mapa%20M.%20en%20C.%20Miguel%20%C3%81ngel%20Le%C3%B3n%20Tapia.jpg)
+# Figura 2. Mapa de Tecolutla Veracruz 
+
+# Los organismos fueron aclimatados en laboratorio de Ecotoxicología, para posteriormente tomar parametros morfometricos, y mantenerlos en condiciones optimás para analís posteriores 
+
+# ![WhatsApp%20Image%202019-07-15%20at%209.50.13%20PM.jpeg](attachment:WhatsApp%20Image%202019-07-15%20at%209.50.13%20PM.jpeg)
+# 
+# Figura 3. Fotografias del procedimeinto 
+
+# Tabla 1. Morfometricos de Anémonas 
+
+# | Caracteres | Discopedio | Disco oral | Coronas | Tentáculos | Hileras columnares |
+# |:----------:|:---------------|-------:|:----------:|:----------:|:----------:|
+# |Organismo 1| 1.75 | 1.2 | 5 | 105 | 90 | 
+# |Organismo 2| 1.71 | 1.3 | 5 | 97 | 80 |
+# |Organismo 3| 1.0 | 0.9 | 5 | 82 | 78  |
+# |Organismo 4| 2.2 | 1.2 | 5 | 116 | 90 |
+# |Organismo 5| 1.67 | 1.2 | 5 | 104 | 90 |
+# |Organismo 6| 1.8 | 1.2 | 5 | 112 | 90 |
+# |Organismo 7| 2.3 | 1.17 | 5 | 112 | 88 |
+# |Organismo 8| 2.0 | 1.3 | 5 | 99 | 90 |
+# |Organismo 9| 2.4 | 1.0 | 5 | 97 | 90 |
+# |Organismo 10| 2.2 | 1.3 | 5 | 98 | 90 |
+# |Media| 1.9 | 1.17 | 5 | 101 | 87.6 |
+# |Desv. Estándar| 0.41 | 0.13 | 5 | 9.29 | 4.59 |
+
+#  # Método Histología 
+
+# **Anestesia** Se seleccionaros a dos anémonas para anestesiarlas y se probaron dos métodos de anestesia, uno con cristales de mentol y sales de magnesio, y otro bajando la temperatura gradualmente, con el fin de tener a los organismos en completa relajación. El método con el cual se logró el objetivo fue el segundo, ya que con el de cristales de mentol en menos de 20 min el organismo comenzó a irritarse, desprendiendo un mucus en todo su cuerpo 
+
+# ![WhatsApp%20Image%202019-07-15%20at%2010.41.06%20PM.jpeg](attachment:WhatsApp%20Image%202019-07-15%20at%2010.41.06%20PM.jpeg)
+# 
+# Figura 4. Anémonas anestesiadas 
+
+# Posteriormente se obtuvieron las seccines , para su procesamiento y analisis tisular H-E/H-F, haciendo algunas modificaciones al protocolo de tinción 
+
+# ![WhatsApp%20Image%202019-07-15%20at%2011.29.48%20PM.jpeg](attachment:WhatsApp%20Image%202019-07-15%20at%2011.29.48%20PM.jpeg)
+# 
+# Figura 5. Fotografias del proceso 
+
+# # Método Molecular 
+
+# **Extracción DNA** De las muestras se decidió que la mejor región de la anémona para extraer DNA es el disco pedio, ya que es dónde había menos interferencia de zooxantelas. 
+# Del kit **PROMEGA** se realizó la siguiente modificación: 
+# **Agregar 10 μL más de la proteinasa K ya que el tejido no se degrado lo suficiente**
+# Nuestra muestra tardo mucho en degradarse, el tejido estaba muy pegajoso, y tal vez esto interfirió en nuestro resultado para electroforesis y PCR. 
+# 
+
+# ![20170310_131125.jpg](attachment:20170310_131125.jpg) 
+# 
+# Figura 6. Fotografía de disco pedio de anémona 
+# 
+
+# **Cuantificación de DNA** Se realizó en laboratorio de Biología Molecular con el equipo **NanoDrop 2000. Scientific, T (2016)** con los siguiente criterios para interpretar los resultados
+
+# ![WhatsApp%20Image%202019-07-10%20at%209.10.59%20AM.jpeg](attachment:WhatsApp%20Image%202019-07-16%20at%209.10.59%20AM.jpeg)
+# 
+# Figura 7. NonoDrop 2000, Scientific, T (2016)
+# 
+
+# Tabla 2. Criterios de pureza 
+# 
+# |Relación| Valor|Criterios de Pureza|
+# |:----------:|:---------------|-------:|
+# |A260/280		|1.8-2|ADN pureza óptima|
+# |.               | 1.6-1.8| ADN pureza aceptable|
+# | .              | <1.6 |Prsencia de compuestos aromáticos|
+# |  .             | >2.1| Contaminación con RNA|
+# |A260/230       | 1.5-2.2| ADN pureza óptima|
+# |   .            |<1.5|Contaminación por sales, fenoles, carbohidratos.|
+# 
+
+# Tabla 3. Resultados de Nanodrop
+# 
+# 
+# 
+# |#| Sample ID|Concentración de nucleótidos ng/μL	A260 nm	A280 nm	260/280 nm	260/230 nm|
+# |:----------:|:--------|-----------------------------:|
+# |1	|Anémona 1|55.4   	1.108	  0.658  	1.68	  0.12|
+# |2               | Anémona 2| 26.9	  0.539	  0.326	  1.65	  0.16|
+
+# Los resultados de la lectura indicaron una concentración superior en la muestra 1 comparada con la muestra 2. Para la razón 260/280nm indicaron que el ADN tiene una pureza aceptable debido a la concentracion de acidos nucleicos, pero con la lectura de 260/230nm se indica que el ADN está contaminado con sales, fenoles o carbohidratos ya que las absorbancia esta difractada.
+# 
+# 
+# 
+
+# **Amplificación** La amplificación vía PCR (reacción en cadena de la polimerasa, por sus siglas en inglés) del los gen 16S se realizó según lo descrito por Palumbi et al. (2002) y Folmer et al. (1994), respectivamente, con el kit de amplificación de PROMEGA. Los primers y condiciones del ciclo de PCR que se utilizaron se muestran en la Tabla 1, y en la Tabla 2 se detallan los reactivos que se ocuparon.
+
+# Tabla 4. Primers utilizados para la amplificación y secuenciación del gen 16S.
+# 
+# |Gen | Nombre|Iniciador|
+# |:----------:|:---------------|-------:|
+# |16S|16S AR |5'- CGCCTGTTTATCAAAAACAT3'|
+# |16S| 16S BR| 5'- CCGGTGTGAACTCAGATCACGT3'|
+# 
+# |Desnaturalización inicial| Desnaturalización|Alineación|
+# |:----------:|:---------------|-------:|
+# |94°C por 1´|40°C por 1´|72°C por 1´|
+# |1 ciclo| 35 ciclos| 35 ciclos|
+# 
+# |Extensión| Extensión inicial | 
+# |:----------:|:----------------------:|
+# |72°C por 2´|72°C por 10´|
+# |1 ciclo|1 ciclo|
+# 
+# Tabla 5. Reactivos utilizados en la amplificación de ADN.
+# 
+# |Reactivos| Cantidades µl. | 
+# |:----------:|:----------------------:|
+# |Go taq green master mix|12.5|
+# |Primer 1|1|
+# |Primer 2|1|
+# |H2O|9.5|
+# |Total|24| 
+
+# Se realizó la purificación de DNA con el kit **PROMEGA**
+
+# ![WhatsApp%20Image%202019-07-10%20at%209.07.50%20AM.jpeg](attachment:WhatsApp%20Image%202019-07-16%20at%209.07.50%20AM.jpeg)
+
+# Los productos de extracción, amplificación y purificación se visualizaron y cuantificaron en geles de agarosa al 1%
+# 
+# 
+
+# Para posteriormente mandar a secuenciar **GEN 16S**
+# 
+# 
+# Los productos de PCR purificados se secuenciaron en el Laboratorio Divisional de Biología Molecular de la Universidad Autónoma Metropolitana-Iztapalapa. Las reacciones para producir secuencias marcadas se realizaron empleando el kit de la compañía Perkin Elmer (ABI PRISM® BigDyeTM Terminator Cycle Sequencing Reaction Kit). Los primers usados para la secuenciación fueron los mismos que se utilizaron para la amplificación (Tabla 1). Las muestras fueron cargadas en un Secuenciador “ABI PRISM 3100 Genetic Analyzer” (Applied Biosystems, NJ, USA) para obtener las secuencias nucleotídicas.
+# 
+
+# # ANALISIS BIOINFORMATICO 
+
+# La bioinformática es una nueva disciplina dentro de la biología, donde las herramientas de la computación tienen una función primordial. Si bien algunos restringen el rango de estudio de la bioinformática al manejo y análisis de bases de datos biológicas -principalmente de secuencias-, podría atribuírsele un sentido más amplio, como la fusión de las técnicas computacionales con el entendimiento y apreciación de datos biológicos, el almacenamiento, recuperación, manipulación y correlación de datos procedentes de distintas fuentes (cañedo 2005). 
+# 
+# Hicimos una prueba piloto con las secuencias obtenidas tras la secuenciación de las muestras.
+# 
+
+# # Analisis de archivos ab1 y obtener secuencias consenso.
+# 
+
+# In[8]:
+
+
+cd ~/Desktop/pdi_molecular/data/ab1/
+
+
+# In[9]:
+
+
+ls -lh *.ab1
+
+
+# # Para leer el archivo ab1 se utiliza SeqIO con la opción abi1
+
+# In[10]:
+
+
+record = SeqIO.read('A2a-16S-F.ab1', 'abi')
+record
+
+
+# # El archivo ab1 contiene la información de la secuencia y del electroferograma.
+# Graficado del electroferograma
+
+# In[11]:
+
+
+record.annotations.keys()
+record.annotations['abif_raw'].keys()
+channels = ['DATA9', 'DATA10', 'DATA11', 'DATA12']
+trace = defaultdict(list)
+
+
+# In[12]:
+
+
+for c in channels:
+    trace[c] = record.annotations['abif_raw'][c]
+
+
+# In[13]:
+
+
+graficado (0,7000)
+
+
+# # se define un intervalo menor y se grafica por partes
+
+# In[14]:
+
+
+intervalos = range(1000,8000,1000)
+intervalos
+
+
+# In[15]:
+
+
+for intervalo in intervalos:
+    graficado (intervalo-1000,intervalo)
+
+
+# # Se utilizarán ambas secuencias de un fragmento de interés.
+# ### en este caso:
+# `A2a-16S-F.ab1`  
+# `A2a-16S-R.ab1`
+# # Secuencias con el iniciador sentido (forward) y antisentido (reverse)
+# 
+
+# In[17]:
+
+
+recordtrimmed_F = SeqIO.read('A2a-16S-F.ab1', 'abi-trim')
+recordtrimmed_R = SeqIO.read('A2a-16S-R.ab1', 'abi-trim')
+
+
+# # Se alinean ambos fragmentos para corroborar la secuencia
+
+# In[18]:
+
+
+alignments = pairwise2.align.globalds(recordtrimmed_F.seq,recordtrimmed_R.seq.reverse_complement(), 
+                                      blosum62, -10, -0.5)
+
+
+# # Se muestran los alineamientos obtenidos
+
+# In[19]:
+
+
+for row in range(len(alignments)):
+    print ("Alineamiento", row)
+    print(pairwise2.format_alignment(*alignments[row]), "\n")
+
+
+# Seleccionar el mejor alineamiento en la siguiente celda como:
+# consenso2(alignments[x][0], alignments[x][1])
+# donde x es la secuencia seleccionada.
+# 
+# en este caso se selecciona la # 1
+
+# In[20]:
+
+
+secuencia = consenso2(alignments[1][0], alignments[1][1])
+print(secuencia)
+
+
+# **Referencias**  
+# [Nick FD](https://es.scribd.com/document/354767407/Morandinimedina2012-Capitulo-17-Libro-Arbol-Vida)2012. título del capítulo. En: Editores.El arbol del a vida Editorial. pp
+# 
+# 
+# [Häussermann, V.](https://doi.org/10.1017/S0025315404010215h). 2004. Identification and taxonomy of soft-bodied hexacorals exemplified by Chilean sea anemones; including guidelines for sampling, preservation and examination. J.of the Marine Biological Association of the United Kingdom. 84(5): 931-936
+# 
+# [Cañedo](http://scielo.sld.cu/pdf/aci/v12n6/aci02604.pdf). 2005. Bioinformática: en busca de los secretos moleculares de la vida
